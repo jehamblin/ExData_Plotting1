b0ac9bd7e33b7b795813f7c6dd0f38d4d8dce750
jehamblin <jehamb@ship.edu>
2014-05-05 12:18:44 -0400

    Fixed problem
    Fixed a problem with the (large) data file not uploading to GitHub.  The
    script now reads the file from elsewhere.

--- project1.R
+++ project1.R
@@ -1,4 +1,4 @@
-setwd("C:\\Users\\jehamb\\Documents\\My Dropbox\\Coursera\\Exploratory Data Analysis")
+setwd("C:\\Users\\jehamb\\Desktop")
 
 ## Read in the entire data file
 df <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
@@ -12,6 +12,8 @@ df2 <- df[which(dates == as.Date("2007-02-01") | dates== as.Date("2007-02-02")),
 dt2 <- datesTimes[which(dates == as.Date("2007-02-01") | dates== as.Date("2007-02-02"))]
 
 ## Create plot 1
+setwd("C:\\Users\\jehamb\\Documents\\GitHub\\ExData_Plotting1")
+
 hist(df2$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power")
 dev.copy(png,file="plot1.png")
 dev.off()
